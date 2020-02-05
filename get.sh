58b662dc727389ba6c467b5f225197ded35d3cd1
ababwbq <45261780+unkaer@users.noreply.github.com>
2020-02-05 11:46:38 +0800

    哈鱼矿工

---
+++ get.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+phone=$1
+mode=$2
+unamestr=`uname`
+downurl=""
+mkdir -p hashfish
+cd hashfish
+if [[ "$unamestr" == 'Linux' ]]; then
+   echo "linux"
+   downurl="http://cli.hashfish.net/linux.tar.gz"
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   echo "osx"
+   downurl="http://cli.hashfish.net/darwin.tar.gz"
+fi
+
+curl -o down.tar.gz $downurl
+tar -xvzf down.tar.gz
+rm down.tar.gz
+chmod a+x hashfish
+./hashfish -g -p $phone -m $mode
+./hashfish
