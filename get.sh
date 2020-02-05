e18b298648345ba7e580225ad71aa8cb44648a1e
ababwbq <45261780+unkaer@users.noreply.github.com>
2020-02-05 13:06:11 +0800

    哈鱼矿工

--- get.sh
+++ get.sh
@@ -1,5 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# 本文件地址https://cdn.jsdelivr.net/gh/unkaer/blog-cdn/get.sh
+# 哈鱼Termux脚本
+# curl -s https://cdn.jsdelivr.net/gh/unkaer/blog-cdn/get.sh -o get.sh && bash get.sh ph_r1J0HFOARUlydx0= 0 && rm -f get.sh
+
 phone=$1
 mode=$2
 unamestr=`uname`
